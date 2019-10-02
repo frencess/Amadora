0eb31e6b61a34c8e503ce1d441fadcef38026ef9
frencess <55073005+frencess@users.noreply.github.com>
2019-10-02 11:36:23 +0800

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        Scanner scan= new Scanner (System.in);
+        
+        int num = Integer.parseInt("10");
+        String var = num + "";     
+        String message = "Welcome to Java";
+        System.out.println(message.indexOf('a', 10));
+        
+        
+        System.exit(0);  
+        
+// ---------------       
+//        String var = scan.nextLine();
+//        System.out.println("var");
+        
+////        kun maggamit ka ug equals sa string                 
+//        if (var.equals("Hello"))
+                
+                
+//        String message = "welcome to Java";
+//        String message2 = "any additional message ";
+//        
+//        System.out.println(message.length());
+//        System.out.println(message.charAt(12));
+//        System.out.println(message.concat("for dummies"));
+//        System.out.println(message.toUpperCase());
+//        System.out.println(message.toLowerCase());
+//        System.out.println(message.trim());
+        
+        
+//    ------------    
+//        char x = 77;
+//        System.out.println(x);
+//        
+//        char var = 'a';
+//        System.out.println(Character.isDigit(var));
+//        System.out.println(Character.isLetter(var));
+//        System.out.println(Character.isLetterofDigit(var));
+//        System.out.println(Character.islowerCase(var));
+//        System.out.println(Character.is(var));
+//        System.out.println(Character.isDigit(var));
+        
+    }
+    
+}
